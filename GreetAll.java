b7521a27930d29ca7c4fa6cf1693b7e932bc26ac
gyanendras <gyanendras@gmail.com>
2023-07-20 12:12:11 +0530

    Initial commit Gyanendra

---
+++ GreetAll.java
@@ -0,0 +1,24 @@
+public class GreetAll{
+private String s = "Hello World!";
+private int x = 10;
+
+GreetAll(){
+	s = "HI PPL!" ;
+}
+
+GreetAll(String s){
+	this.s = s ;
+}
+
+void greet(){
+System.out.println(s);
+}
+
+public static void main(String args[]){
+GreetAll ga = new GreetAll();
+GreetAll ga1 = new GreetAll("HI from parameterised Const");
+ga.greet();
+ga1.greet();
+
+}
+}
